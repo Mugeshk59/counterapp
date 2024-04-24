5bccae6830b52673b53f357f22cff91efebcc768
Mugeshk59 <mugeshk59@gmail.com>
2024-04-24 18:56:32 +0530

    completed counterApp task

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { useState } from "react";
+import "./styles.css";
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementHandler = () => {
+    setCounter((prev) => prev + 1);
+  };
+  const decrementHandler = () => {
+    setCounter((prev) => prev - 1);
+  };
+  return (
+    <div className="counterContainer">
+      <h1>Counter App</h1>
+      <p>counter : {counter}</p>
+      <button onClick={incrementHandler}>Increment</button>
+      <button onClick={decrementHandler}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
